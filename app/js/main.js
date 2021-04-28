6ffc59ffc0f9531e18644a8e84db4cd55704619d
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-28 13:54:08 +0600

    add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
 
-
-
+    $('.header__slider').slick({
+        nextArrow:'<button class="header__arrow header__arrow-next"><img src="images/header-arrow-next.png" alt=""></button>',
+        prevArrow:'<button class="header__arrow header__arrow-prev"><img src="images/header-arrow-prev.png" alt=""></button>'
+    });
   
 });
