5aaa320b4400acc602dccbaf369e4801ec1f30e4
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-29 15:09:10 +0600

    add portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,14 @@ $(function(){
         nextArrow:'<button class="header__arrow header__arrow-next"><img src="images/header-arrow-next.png" alt=""></button>',
         prevArrow:'<button class="header__arrow header__arrow-prev"><img src="images/header-arrow-prev.png" alt=""></button>'
     });
-  
+
+ 
+        $('.portfolio__box').masonry({
+            // options
+            itemSelector: '.portfolio__box-img',
+            columnWidth: 10
+        });
+   
+        var mixer = mixitup('.portfolio__inner');
+        mixer.filter('.all');
 });
